f942c842058304aafd5c4bb3930d27c2120eb65f
Nicholaskarl <nkjohns6@asu.edu>
2020-10-28 20:11:49 -0700

    Name changes
    We just made name changes to some of our files to better group them together.

---
+++ finalJavaLogin.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-button");
+
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+       location.replace("FinalMainWebpage.html");
+    } else {
+        window.alert("Incorrect Username or Password please try again");
+    }
+})
